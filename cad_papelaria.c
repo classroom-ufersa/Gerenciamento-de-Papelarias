efde8d49e7c25c4eeea7ff78fb9fa9607b7b6fb4
Pedro Henrique Filgueira Carlos <128182599+PedrohfCarlos@users.noreply.github.com>
2024-04-01 08:05:48 -0300

    att cad papelaria

--- cad_papelaria.c
+++ cad_papelaria.c
@@ -1,30 +1,87 @@
 #include "cad_papelaria.h"
 
-
 typedef struct papelaria Papelaria;
 
-
-void cad_papelaria(Papelaria *papelaria, char cadastro){
+void cad_pap( Papelaria **papelaria, int *quant) {
+    *papelaria = realloc(*papelaria, (*quant + 1) * sizeof(papelaria));
+    if (*papelaria == NULL) {
+        printf("Erro ao alocar memória.\n");
+        exit(1);
+    }
     printf("Insira o nome da nova papelaria: \n");
-    scanf(" %[^\n]", papelaria->nome);
+    scanf(" %[^\n]", (*papelaria)[*quant].nome);
     printf("Insira o endereco da papelaria: \n");
-    scanf(" %[^\n]", papelaria->local);
+    scanf(" %[^\n]", (*papelaria)[*quant].local);
     printf("Informe os produtos presentes nessa papelaria: \n");
-    scanf(" %[^\n]", papelaria->produtos);
+    scanf(" %[^\n]", (*papelaria)[*quant].produtos);
+     (*quant)++;
 }
-
-void rem_papelaria(Papelaria *papelaria, int qtdpapelaria){
-    char rempapel[50];
-    printf("Qual papelaria deseja remover: \n");
-    scanf("%s",rempapel);
-    for (int i = 0; i<qtdpapelaria; i++){
-        if(strcmp(papelaria[i].nome,rempapel) == 0){
-            printf("papelaria removida: %s \n", papelaria[i].nome);
+    void rem_pap(Papelaria *papelaria, int *quant, char *nomePap) {
+    int i, j;
+    for (i = 0; i < *quant; i++) {
+        if (strcmp(papelaria[i].nome, nomePap) == 0) {
+           
+            for (j = i; j < *quant - 1; j++) {
+                strcpy(papelaria[j].nome, papelaria[j + 1].nome);
+                strcpy(papelaria[j].local, papelaria[j + 1].local);
+                strcpy(papelaria[j].produtos, papelaria[j + 1].produtos);
+            }
+            (*quant)--;
+            break;
         }
+    }
+    FILE *arquivo = fopen("papelarias.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    for (i = 0; i < *quant; i++) {
+        fprintf(arquivo, "Nome: %s | Local: %s | Produtos: %s \n", papelaria[i].nome, papelaria[i].local, papelaria[i].produtos);
+    }
+    fclose(arquivo);
+
+    printf("Papelaria %s removida com sucesso.\n", nomePap);
+}
+void pap_salva(Papelaria *papelaria, int quant){
+    FILE *arquivo = fopen("papelarias.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < quant; i++) {
+        fprintf(arquivo, "Nome: %s | Local: %s | Produtos: %s \n ", papelaria[i].nome, papelaria[i].local, papelaria[i].produtos);
+    }
+
+    fclose(arquivo);
+}
+void carr_pap(Papelaria **papelaria, int *quant) {
+   FILE *arquivo = fopen("papelarias.txt", "r");
+    if (arquivo == NULL) {
+        printf("Arquivo de papelarias não encontrado.\n");
+        return;
+    }
+
+    while (!feof(arquivo)) {
+        *papelaria = realloc(*papelaria, (*quant + 1) * sizeof(Papelaria));
+        if (*papelaria == NULL) {
+            printf("Erro ao alocar memória.\n");
+            exit(1);
+        }
+        fscanf(arquivo, "%[^|]|%[^|]|%[^\n]\n", (*papelaria)[*quant].nome, (*papelaria)[*quant].local, (*papelaria)[*quant].produtos);
+        (*quant)++;
+    }
+
+    fclose(arquivo);
+}
+void listar_pap(Papelaria *papelaria, int quant){
+     printf("\nDados fornecidos:\n");
+    for (int i = 0; i < quant; i++) {
+        printf("Papelaria %d\n", i + 1);
+        printf(" Nome: %s \n Local: %s\n Produtos: %s\n", papelaria[i].nome, papelaria[i].local, papelaria[i].produtos);
+        printf("--------------------------\n");
+       
 
     }
 }
 
-void listar_pap(){
-
-}
