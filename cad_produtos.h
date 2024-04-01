0639cc2a0e8ba5cb97ceeff87e77c28b4362e028
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-04-01 12:49:13 -0300

    att .h

--- cad_produtos.h
+++ cad_produtos.h
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef struct produto {
 char nome_prod [70];
 char tip_prod [70];
 float preco;
-int quant;
+int qtd;
 }Produto;
 
 // Função para cadastrar uma produto
-void adiciona_produto(Produto *produto);
-// Função para renover um produto 
-void remover_produto();
+void ad_produto(Produto **produto, int *qtdpod);
 
-void comprar_produtos();
+// Função para renover um produto 
+void remover_produto(Produto *produto, int *qtdprod, char nomeprod);
+
+// Função para adquirir produtos para papelaria
+// Essa fução se encontrara na opção repor estoque
+void venda_prod();
+
 // Função para buscar produtos cadastrados em uma papelaria
-void busca_produto();
+void busca_prod(Produto *produto, int *qtdpod, char nomepod);
