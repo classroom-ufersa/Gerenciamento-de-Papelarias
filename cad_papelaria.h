6e96fe5ec7d7ecad6bf11774ce87a26ce7e8c59e
Pedro Henrique Filgueira Carlos <128182599+PedrohfCarlos@users.noreply.github.com>
2024-04-01 08:06:57 -0300

    att papelaria.h

--- cad_papelaria.h
+++ cad_papelaria.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef struct papelaria {
 char nome [50];
 char local [100];
@@ -8,8 +9,12 @@ char produtos [100];
 }Papelaria;
 
 // Função para cadastrar papelaria
-void cad_papelaria(Papelaria *papelaria, char cadastro);
-//Função para remover papelaria
-void rem_papelaria(Papelaria *papelaria, int qtdpapelaria);
+void cad_pap( Papelaria **papelaria, int *quant);
+//Função para remover papelaria;
+void rem_pap(Papelaria *papelaria, int *quant, char *nomePap);
+
+void pap_salva(Papelaria *papelaria, int quant);
+
+void carr_pap(Papelaria **papelaria, int *quant);
 // Função para lista as papelaria cadastradas
-void listar_pap();
+void listar_pap(Papelaria *papelaria, int quant);
