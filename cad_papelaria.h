fb3ee16e65083de02043062aeb729f5e6afbfd81
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-04-12 21:56:01 -0300

    comentando pap.h

--- cad_papelaria.h
+++ cad_papelaria.h
@@ -17,18 +17,21 @@ struct No{
      struct  No * porx;
     };
 
-void cad_pap( Papelaria **papelaria, Lista *plista) ;
-//Função para remover papelaria;
+
+// Função para cadastrar papelaria
+void cad_pap( Papelaria **papelaria);
+
+// Função para remover papelaria;
 void inserir(Lista *plista, Papelaria dado);
 
+// Função para remover papelaria escolhida junto aos produtos
 void deletar_pap (Lista *plista, char* nome);
 
+// Lista papelarias existentes junto aos produtos
 void listar_pap(Lista lista);
 
+// Struct responsavel pela busca das papelarias
 struct No* pesquisar(Lista lista, char* nome);
 
-void salva_dados(Lista *plista);
 
-void carregar_dados(Lista *plista);
 
-void atualizar_arquivo(Lista *plista);
