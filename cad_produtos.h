c039ba4051f0ce581c899edb7ce6a20793063a3b
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-04-12 21:56:51 -0300

    Concerto func_rem_prod

--- cad_produtos.h
+++ cad_produtos.h
@@ -12,11 +12,13 @@ int qtd;
 // Função para cadastrar uma produto
 void ad_produto(Produto **produto, int *qtdpod);
 
-// Função para renover um produto 
-void remover_produto(Produto *produto, int *qtdprod, char nomeprod);
+// Função para remover um produto 
+void remover_produto(Produto *produto, int *qtdprod, char nomeprod[]);
 
-// Função para adquirir produtos para papelaria
-// Essa fução se encontrara na opção repor estoque
+/*
+Essa fução se encontrara na opção repor estoque
+Função para adquirir produtos para papelaria
+*/
 void venda_prod();
 
 // Função para buscar produtos cadastrados em uma papelaria
