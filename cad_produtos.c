46b1ee8e257e40cd3c81da207ece80509908156e
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-04-13 02:34:39 -0300

    new funcs prod.c

--- cad_produtos.c
+++ cad_produtos.c
@@ -2,25 +2,63 @@
 
 typedef struct Produto produto; 
 
-void busca_prod(Produto *produto, int *qtdpod, char nomepod){
-
+struct No* pesq_prod(lista_prod lista, char* nome_prod){
+    struct No* pro_it;
+     for (pro_it = lista.inicio; pro_it != NULL; pro_it = pro_it->proximo) {
+        if (strcasecmp(pro_it->dado.nome_prod, nome_prod) == 0) {
+            return pro_it;
+        }
+    }
+    return NULL; 
 }
 
 void ad_produto(Produto **produto, int *qtdpod){
     *produto = realloc(*produto, (*qtdpod + 1) *sizeof(produto));
-        if(*produto == NULL){
-            printf("erro alocar memoria \n");
-            exit(1);
-        }
+    if(*produto == NULL){
+        printf("erro alocar memoria \n");
+        exit(1);
+    }
+    do {
     printf("Informe o nome do produto: \n");
     scanf(" %[^\n]", (*produto)[*qtdpod].nome_prod);
+        while((getchar()) != '\n');
+        if(pesq_prod(*plista, (*Produto)->nome_prod) != NULL){
+            system ("cls");
+            printf("Esse produto ja foi cadastrado %s por favor, informe um outro produto\n", (*Produto)->nome);
+        }
+    }while(pesq_prod(*plista, (*produto)->nome_prod) !=NULL);
     printf("Insira o tipo do produto: \n");
     scanf(" %[^\n]", (*produto)[*qtdpod].tip_prod);
     printf("Insira um preco para o produto: \n");
     scanf("%f", (*produto)[*qtdpod].preco);
     printf("Informe a quantidade desse produto: \n");
     scanf("%d", (*produto)[*qtdpod].qtd);
+}
 
+void salva_prod(Lista_prod *plista) {
+    FILE *arquivo = fopen ("produto.txt"," w+");
+    if (arquivo==NULL){
+    printf("Erro ao abri o arquivo.\n");
+    exit(1);
+    }
+struct  No *pro_it; 
+for (pro_it = plista->inicio; pro_it != NULL; pro_it = pro_it -> proximo){
+    fprintf(arquivo,"Produto: %s \n Tipo: %s \n Preco: %f  \n Quantidade: %d", pro_it->dado.nome_prod, pro_it->dado.tip_prod, pro_it->preco, pro_it->qtd);
+
+}
+fclose(arquivo);
+}
+
+void insere_prod(Lista_prod *plista, Dados Produto){
+    struct No *novo = (struct No*) malloc(sizeof(struct No));
+if (novo == NULL){
+    printf("Erro ao alocar memoria\n");
+    exit(1);
+}
+novo-> Produto = Produto;
+novo-> proximo = plista->inicio;
+plista->inicio = novo;
+salva_dados(plista);
 }
 
 void remover_produto(Produto *produto, int *qtdprod, char nomeprod[]) {
@@ -55,3 +93,4 @@ void remover_produto(Produto *produto, int *qtdprod, char nomeprod[]) {
 void venda_prod(){
    
 }
+
