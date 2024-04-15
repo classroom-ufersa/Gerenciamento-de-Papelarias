7074915467e84b6bd6cac08dd09950ab82e43e12
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-04-15 01:32:17 -0300

    Update chuva

--- cad_papelaria.c
+++ cad_papelaria.c
@@ -12,9 +12,10 @@ void cad_pap(papelaria *dadopapelaria , Lista *plista){
     } while(pesquisar(*plista, dadopapelaria->nome) != NULL);
     printf("Insira o endereco da papelaria: \n");
     scanf(  "%[^\n]",dadopapelaria->local);
-        dadopapelaria->produtos = malloc(sizeof(Produto) * 10);
+        dadopapelaria->produtos = malloc(sizeof(Produto) * 1000);
     dadopapelaria->num_produtos = 0;
 }
+
 // função para inserir os dados da papelaria na lista
 void inserir_lista(Lista *plista , papelaria dado){
   No *novo = (struct No*) malloc(sizeof( No));
@@ -26,7 +27,8 @@ void inserir_lista(Lista *plista , papelaria dado){
     novo->proximo = NULL;
     if (plista->inicio == NULL){
         plista->inicio = novo;
-    } else {
+    }
+    else{
       No* pi;
         for(pi = plista->inicio; pi->proximo != NULL; pi = pi ->proximo);
         pi->proximo = novo;
@@ -37,7 +39,7 @@ void inserir_lista(Lista *plista , papelaria dado){
 void mostrar(Lista lista){
   No* pi;
     for(pi = lista.inicio; pi != NULL; pi = pi->proximo){
-        printf("Nome: %s|\t Endereco: %s|\n", pi->dado.nome, pi->dado.local);
+    printf("Nome: %s|\t Endereco: %s|\n", pi->dado.nome, pi->dado.local);
     }
 }
 
@@ -52,46 +54,51 @@ void mostrar(Lista lista){
 }
 
     
-    void deletar_pap (Lista *plista, char* nome){
-        if (plista->inicio ==NULL){
-            printf("Sem papelaria\n");
-        } else if(strcmp(plista->inicio->dado.nome, nome) == 0) {
-            struct No* pi = plista->inicio;
-            plista->inicio = pi ->proximo;
-            free(pi);    
-        }else if (plista->inicio->proximo==NULL){
-            if  (strcmp(plista->inicio->dado.nome, nome) != 0){
+void deletar_pap (Lista *plista, char* nome){
+    if (plista->inicio ==NULL){
+        printf("Sem papelaria\n");
+    } else if(strcmp(plista->inicio->dado.nome, nome) == 0) {
+        struct No* pi = plista->inicio;
+        plista->inicio = pi ->proximo;
+        free(pi);    
+    }else if (plista->inicio->proximo==NULL){
+        if  (strcmp(plista->inicio->dado.nome, nome) != 0){
                 printf("A papelaria nao pode ser removida\n");
-            }
-        }else {
-            struct No * pa;
-            struct No * pi;
-            for (pi = plista->inicio; pi != NULL && strcmp(pi->dado.nome, nome) != 0; pi = pi->proximo){
-                pa = pi;
-            } if (pi == NULL){
-                printf(" A papelaria nao pode ser renovida\n");
-            } else {
-                pa->proximo = pi->proximo;
-                free(pi);
-                  free(pi->dado.produtos);
-            }         
         }
-        atualizar_arquivo(plista);
+    }else {
+        struct No * pa;
+        struct No * pi;
+        for (pi = plista->inicio; pi != NULL && strcmp(pi->dado.nome, nome) != 0; pi = pi->proximo){
+        pa = pi;
+        } if (pi == NULL){
+        printf(" A papelaria nao pode ser removida\n");
+        } else {
+        pa->proximo = pi->proximo;
+        free(pi);
+        free(pi->dado.produtos);
+        }         
     }
+    atualizar_arquivo(plista);
+}
 
 void salva_dados(Lista *plista) {
+    
     FILE *file = fopen ("papelaria.txt"," w");
     if (file==NULL){
-    printf("Erro ao abri o arquivo.\n");
-    exit(1);
+    printf("Erro ao abrir o arquivo.\n");
+    return;
     }
- No *pi; 
-for (pi = plista->inicio; pi != NULL; pi = pi -> proximo){
-    fprintf(file,"Nome %s |Endereco %s \n", pi->dado.nome, pi->dado.local);
-
-}
+int num_nos = 0;
+    for (No* temp = plista->inicio; temp != NULL; temp = temp->proximo) {
+        num_nos++;
+    }
+    for (No* atual = plista->inicio; num_nos > 0; atual = atual->proximo, num_nos--) {
+        fprintf(file, "Papelaria: %s| Endereco: %s \n", atual->dado.nome, atual->dado.local);
+        for (int i = 0; i < atual->dado.num_produtos; i++) {
+            fprintf(file, "Produto: %s| Tipo: %s| \n Preco por quantidade: %.2f| Quantidade: %d\n",atual->dado.produtos[i].nome_prod,atual->dado.produtos[i].tip_prod, atual->dado.produtos[i].preco,atual->dado.produtos[i].qtd);
+        }
+    }
 fclose(file);
-
 }
 
 void carregar_dados(Lista *lista) {
@@ -108,8 +115,9 @@ void carregar_dados(Lista *lista) {
             }
             inserir_lista(lista, dadopapelaria);
         }
-         fclose(file);
-    }   
+    fclose(file);
+}   
+
 void atualizar_arquivo(Lista *plista){
     FILE *file =fopen("papelaria.txt","w");
       if (file==NULL){
@@ -119,9 +127,10 @@ void atualizar_arquivo(Lista *plista){
      No *pi; 
 for (pi = plista->inicio; pi != NULL; pi = pi -> proximo){
     fprintf(file,"Nome %s Endereco %s \n", pi->dado.nome, pi->dado.local);
- }
-fclose(file);
+    }
+    fclose(file);
 }
+
 void obter_opcao_valida(int *opc) {
     int result;
     do {
