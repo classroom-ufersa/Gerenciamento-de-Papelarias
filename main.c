3c89d84c62fa91ddd6b093526bdf3edcee2f7a50
Romulolevy <125319115+Romulolevy@users.noreply.github.com>
2024-04-15 01:34:04 -0300

    Update main.chuva

--- main.c
+++ main.c
@@ -11,8 +11,7 @@ papelaria dadopapelaria;
 int opc, op2, op3, qtdpod, qtdprod, result;
 char nome[50];
 char nome_prod[50];
-
-carregar_dados(&lista);
+char rem_prod[50];
 
 do {
            printf("***************[ SISTEMA OS EMPAPELADOS ]***************\n");
@@ -26,92 +25,92 @@ obter_opcao_valida(&opc);
         obter_opcao_valida(&op2);
             switch(op2){
                 case 1:{
-                    papelaria dadopap;
-                    cad_pap (&dadopap, &lista);
+                papelaria dadopap;
+                cad_pap (&dadopap, &lista);
                 inserir_lista(&lista, dadopap);
                 salva_dados(&lista);
                 }
-              break;  
+            break;  
 
                 case 2:{
-                  char nome[50];
-                    ((getchar()) != '\n');
-                printf("informe o nome da papelaria\n");
+                char nome[50];
+                ((getchar()) != '\n');
+                printf("Informe o nome da papelaria\n");
                 scanf("%[^\n]",nome);
                 struct No * pi = pesquisar(lista,nome);
                 if (pi !=NULL){
                 printf("%s %s\n", pi->dado.nome, pi->dado.local);
               }
               else {
-                printf("paprarlia nao encontrada\n");
+                printf("Ops :-( | Papelaria nao foi encontrada \n");
+                }
               }
-                }
-                break;
+            break;
 
-                case 3:  
-                 mostrar(lista);
-                break;
+            case 3:  
+              mostrar(lista);
+            break;
 
-                case 4: {
-                  char nome[50];
-               ((getchar()) != '\n');
-                printf("informr a papelaria que deseja Remover\n");
-                scanf("%[^\n]",nome);
-                deletar_pap(&lista, nome);
-                atualizar_arquivo(&lista);
+            case 4: {
+              char nome[50];
+              ((getchar()) != '\n');
+              printf("Informe qual papelaria deseja Remover\n");
+              scanf("%[^\n]",nome);
+              deletar_pap(&lista, nome);
+              atualizar_arquivo(&lista);
+                  }
+              break;
                 }
-                break;
-            }
             } 
         else if(opc == 2){
         printf("------------ OQUE DESEJA FAZER ------------\n");
         printf("(1)-Buscar Produto. \n (2)-Adiocionar novo Produto. \n (3)-Remover um produto existente. \n (4)-Repor estoque. \n");
-          obter_opcao_valida(&op3);
+        obter_opcao_valida(&op3);
             switch(op3){
-                case 1:{
-                   char nome[50];
-                printf("informe o nome do Produto\n");
-                 ((getchar()) != '\n');
-                scanf("%[^\n]",nome);
-                  struct Nop* pro_it = pesq_prod(lista_prod, nome_prod); 
-                  if (pro_it !=NULL){
-                 printf("Produto: %s | Preco: %.2f | Quantidade: %d | \n", pro_it->Dados.nome_prod, pro_it->Dados.preco,pro_it->Dados.tip_prod);
-              }
+              case 1:{
+              char nome[50];
+              printf("informe o nome do Produto\n");
+              ((getchar()) != '\n');
+              scanf("%[^\n]",nome);
+              struct Nop* pro_it = pesq_prod(lista_prod, nome_prod); 
+              if (pro_it !=NULL){
+              printf("Produto: %s | Preco: %.2f | Quantidade: %d | \n", pro_it->Dados.nome_prod, pro_it->Dados.preco,pro_it->Dados.tip_prod);
+              }   
               else {
-                printf("Produto nao encontrada\n");
+              printf("Produto nao encontrado\n");
+                }
               }
-                }
-                  break;
-   
-                break; 
+              break; 
 
-                case 2:
-                produto = malloc(sizeof(Produto));
-                if(produto == NULL){
-                    printf("erro alocar memoria \n");
-                    exit(1);
-                }
-                  ad_produto(&produto, &lista_prod, &lista);
-                insere_prod(&lista_prod, *produto);
-                break;
+              case 2:
+              produto = malloc(sizeof(Produto));
+              if(produto == NULL){
+              printf("erro ao alocar memoria \n");
+              exit(1);
+              }
+              ad_produto(&produto, &lista_prod, &lista);
+              insere_prod(&lista_prod, *produto);
+              salva_dados(&lista);
+              break;
 
-                case 3:
-                remover_produto( produto, &qtdprod, nome_prod);
-                break; 
+              case 3:{
+              printf("Informe o produto que deseja remover: \n");
+              scanf(" %[^\n]", rem_prod);
+              remover_produto(&lista_prod , nome_prod);
+              }
+              break; 
                 
-                case 4:
+              case 4:
                
-                break;
+              break;
 
-            }
+          }
     }
     else if(opc == 3){
-      mostrar_prod(&lista);
-         }
+    mostrar_prod(&lista);
+      }
     }while(opc != 4);
    
-  
- 
     return 0;
 }
  
